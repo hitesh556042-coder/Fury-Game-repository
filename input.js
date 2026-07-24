faf06acace344cfc32f85b0cf34b1fde1c3474f9
hitesh556042-coder <hitesh556042@gmail.com>
2026-07-24 05:52:13 +0530

    input.js

--- input.js
+++ input.js
@@ -1,1 +1,19 @@
+export class InputManager {
+    constructor() {
+        this.keys = {};
+        this.touch = { moveX: 0, moveY: 0, isFiring: false };
 
+        window.addEventListener('keydown', (e) => this.keys[e.key] = true);
+        window.addEventListener('keyup', (e) => this.keys[e.key] = false);
+
+        this.setupMobileControls();
+    }
+
+    setupMobileControls() {
+        const fireBtn = document.getElementById('mobile-fire');
+        if (fireBtn) {
+            fireBtn.addEventListener('touchstart', () => this.touch.isFiring = true);
+            fireBtn.addEventListener('touchend', () => this.touch.isFiring = false);
+        }
+    }
+}
