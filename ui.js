4ae46292d22dab8886039047bc706c4dcc2b0244
hitesh556042-coder <hitesh556042@gmail.com>
2026-07-24 06:29:51 +0530

    ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,20 @@
+class UIManager {
+    constructor() {
+        this.healthBar = document.getElementById('health-bar');
+        this.ammoText = document.getElementById('ammo-text');
+        this.crosshair = document.getElementById('crosshair');
+    }
 
+    updateHUD(health, ammo) {
+        if (this.healthBar) this.healthBar.style.width = health + '%';
+        if (this.ammoText) this.ammoText.innerText = `${ammo} / 30`;
+    }
+
+    toggleScope(isScoped) {
+        if (isScoped) {
+            this.crosshair.classList.remove('hidden');
+        } else {
+            this.crosshair.classList.add('hidden');
+        }
+    }
+}
